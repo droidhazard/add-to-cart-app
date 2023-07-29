15fb4e3a515685b83d45ec0f7aec6dc925916c76
Zohaib <www.zohaib.0335@gmail.com>
2023-07-29 22:29:17 +0500

    update

---
+++ index.js
@@ -0,0 +1,6 @@
+const addButtonEl = document.getElementById('add-button')
+const inputFieldEl = document.getElementById('input-field')
+addButtonEl.addEventListener('click', () => {
+  let inputValue = inputFieldEl.value
+  console.log(inputValue)
+})
